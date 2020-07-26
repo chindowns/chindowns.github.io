e3a71adb77fd61c7518f69e4af0b6630ec1d61b0
ChinDowns <webdev@chindowns.com>
2020-07-26 15:50:32 -0700

    Center and alter Project Formatting

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,7 +1,8 @@
 $.getJSON("./assets/db/projects.json", db => {
 
     db.projects.forEach((project, i) => {
-        $("#projects").append(`
+        i % 2 != 0 ?
+            $("#projects").append(`
             <tr>
                 <td>
                     <h3><a href="${project.url}" target="blank">${project.title}</a></h3>
@@ -12,15 +13,33 @@ $.getJSON("./assets/db/projects.json", db => {
                 <td>
                     <a class="fab fa-github fa-2x" href="${project.github}" SameSite=None;Secure target="blank"></a>
                     <a class="fa fa-link fa-2x" href="${project.url}" SameSite=None;Secure target="blank"></a>
-                    <a href="${project.url}" target="blank"><img src="${project.image}" width="400px" SameSite=None;Secure ></a>
+                    <a href="${project.url}" target="blank"><img class="project-img" src="${project.image}" width="400px" SameSite=None;Secure ></a>
                     <div id="project-skills-${i}" class="skills"></div>
                 </td>
             </tr>
-    `)  
+    `)
+            :
+    $("#projects").append(`
+            <tr>
+                <td>
+                    <a class="fab fa-github fa-2x" href="${project.github}" SameSite=None;Secure target="blank" ></a>
+                    <a class="fa fa-link fa-2x" href="${project.url}" SameSite=None;Secure target="blank"></a><br />
+                    <a href="${project.url}" target="blank"><img class="project-img" src="${project.image}" width="400px" SameSite=None;Secure ></a>
+                    <div id="project-skills-${i}" class="skills"></div>
+                </td>
+                <td>
+                    <h3><a href="${project.url}" target="blank">${project.title}</a></h3>
+                    <p>${project.description}</p>
+                    <h5>Learning</h5>
+                    <p>${project.learning}</p>
+                </td>
+            </tr>
+    `)
+
         project.skills.forEach(skill => addSkill(skill, i))
     })
 });
 
-function addSkill(skill, i){
-    $("#project-skills-"+i).append(`<div class="btn btn-secondary skill">${skill}</div>`);
+function addSkill(skill, i) {
+    $("#project-skills-" + i).append(`<div class="btn btn-secondary skill">${skill}</div>`);
 }
