3f5465163c0ae04b5bcc31d1914ad6c1794650fc
ChinDowns <michael@chindowns.com>
2020-11-17 15:30:13 -0800

    Fix Nav FontAwesome Icons

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,16 +8,16 @@ const nav = `
           <a id="nav-blog" href="#">Blog</a></li>
         <span class="d-flex flex-nowrap" style="font-size: 25px; color: darkgrey ">
           <li class="nav-item">
-            <a class="nav-link fab fa-linkedin" target="blank" href="https://www.linkedin.com/in/michaeldownssj" SameSite=None;Secure ></a>
+            <a class="fab fa-linkedin" target="blank" href="https://www.linkedin.com/in/michaeldownssj" SameSite=None;Secure ></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link fab fa-github-square" target="blank" href="https://github.com/chindowns" SameSite=None;Secure ></a>
+            <a class="fab fa-github-square" target="blank" href="https://github.com/chindowns" SameSite=None;Secure ></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link fas fa-envelope" target="blank" href="mailto://michael@chindowns.com" SameSite=None;Secure ></a>
+            <a class="fas fa-envelope" target="blank" href="mailto://michael@chindowns.com" SameSite=None;Secure ></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link far fa-file-pdf" target="blank" href="./assets/Michael_Downs-FullStack-Resume.pdf" SameSite=None;Secure ></a>
+            <a class="far fa-file-pdf" target="blank" href="./assets/Michael_Downs-FullStack-Resume.pdf" SameSite=None;Secure ></a>
           </li>
         </span>
       </ul>
